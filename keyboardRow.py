d8a2e226a1ceaf889c9ed9e14a3709cd3869e6b9
Vishwanath Hiremath <vishwah1312@gmail.com>
2018-12-13 00:33:45 -0800

    Update keyboardRow.py

--- keyboardRow.py
+++ keyboardRow.py
@@ -1,22 +1,20 @@
 class Solution(object):
-    def check(self,keys,c):
-        for i in xrange(len(keys)):
-            if c in keys[i]:
-                return i
     def findWords(self, words):
         """
         :type words: List[str]
         :rtype: List[str]
         """
-        
-        res=[]
+        dct={}
         keys=["qwertyuiop","asdfghjkl","zxcvbnm"]
+        for i in xrange(len(keys)):
+            for z in keys[i]:
+                dct[z]=i
+        res=[]
         for i in words:
-            x=self.check(keys,i[0].lower())
-            
+            x=dct[i[0].lower()]
             p=1
             for z in i[1:]:
-                if self.check(keys,z.lower())==x:
+                if dct[z.lower()]==x:
                     p+=1
                 else:
                     break
@@ -24,4 +22,3 @@ class Solution(object):
             if p==len(i):
                 res.append(i)
         return res
-                 
